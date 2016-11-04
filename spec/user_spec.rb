f576cb79f63a537db1f3530dcf2cc3ff7cdcd1ea
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-04 10:59:41 -0700

    Create a before call to make the tests a bit more DRY.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,16 +1,18 @@
 require "rails_helper"
 
 RSpec.describe User, :type => :model do
-  it "orders by last name" do
-    lindeman = User.create!(first_name: "Andy", last_name: "Lindeman", password: "password", email: "tester@test.com")
-    chelimsky = User.create!(first_name: "David", last_name: "Chelimsky", password: "password", email: "tester2@test.com")
 
+  before(:each) do
+    @lindeman = User.create!(first_name: "Andy", last_name: "Lindeman", password: "password", email: "tester@test.com")
+    @chelimsky = User.create!(first_name: "David", last_name: "Chelimsky", password: "password", email: "tester2@test.com")
+  end
+
+  it "creates correct amount based on before call" do
     expect(User.count).to eq(2)
   end
 
   it "allows the phone entry to be blank" do
-    lindeman = User.create!(first_name: "Andy", last_name: "Lindeman", password: "password", email: "tester@test.com")
-    expect(lindeman.phone).to eq(nil)
+    expect(@lindeman.phone).to eq(nil)
   end
 
 end
