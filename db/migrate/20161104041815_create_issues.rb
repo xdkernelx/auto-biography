ece6b8edbb7235689cb49ccfaf492aaf20037353
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 21:23:34 -0700

    Update the migration file for Issue, was missing the user_id. Very crucial.

--- db/migrate/20161104041815_create_issues.rb
+++ db/migrate/20161104041815_create_issues.rb
@@ -1,6 +1,7 @@
 class CreateIssues < ActiveRecord::Migration[5.0]
   def change
     create_table :issues do |t|
+      t.integer :user_id
       t.string :title, { limit: 64 }
       t.string :description
       t.integer :urgency
