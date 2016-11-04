5fb31940b6c98f4149955fbd43ad616fbdcc35d7
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-04 14:21:04 -0700

    Create files post-Repair model generation/

---
+++ app/models/repair.rb
@@ -0,0 +1,2 @@
+class Repair < ApplicationRecord
+end
---
+++ db/migrate/20161104212052_create_repairs.rb
@@ -0,0 +1,8 @@
+class CreateRepairs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :repairs do |t|
+
+      t.timestamps
+    end
+  end
+end
