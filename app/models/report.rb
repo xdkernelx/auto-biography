1f7401ead56710a3bf175f20d86ea5240801b715
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 21:00:12 -0700

    Write validations for the Report model.

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,6 @@
 class Report < ApplicationRecord
+
+  validates :title, :description, :date_completed, { presence: true }
+  validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
+
 end
