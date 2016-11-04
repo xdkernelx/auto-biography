5934492061b93aafaedb4aecbb1072895829a932
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 20:40:03 -0700

    Write the migration fields for the Shop model.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,15 @@
 class Shop < ApplicationRecord
+
+  create_table :shops do |t|
+    t.integer :owner_id
+    t.string :name, { limit: 64 }
+    t.string :summary
+    t.string :specialties
+    t.string :address, { limit: 128 }
+    t.string :city, { limit: 64 }
+    t.string :state, { limit: 2 }
+
+    t.timestamps null: false
+  end
+
 end
