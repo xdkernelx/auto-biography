acc8560b50270b9efa6f791492a3d5034ea439e0
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-04 15:02:12 -0700

    Change the call to make it into a before(:all)

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -2,18 +2,19 @@ require "rails_helper"
 
 RSpec.describe Car, :type => :model do
 
-  before(:each) do
+  before(:all) do
     @lindeman = User.create!(first_name: "Andy", last_name: "Lindeman", password: "password", email: "tester@test.com")
     @star = Car.create!(user_id: 1, mileage: 100, vin: "11111111111111111")
-    @dust = Car.create!(user_id: 2, mileage: 20, vin: "11111111111111119")
+    @dust = Car.create!(user_id: 1, mileage: 20, vin: "11111111111111119")
   end
 
   it "creates correct amount based on before call" do
     expect(Car.count).to eq(2)
   end
 
-  it "allows the phone entry to be blank" do
-    expect(@star.phone).to eq(nil)
+  it "correctly associates the user" do
+    p @star.user
+    expect(@star.user).to eq(@lindeman)
   end
 
 end
