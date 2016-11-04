44f53a162303e6ebfac0e525650c2ab14a4350b8
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-04 14:17:02 -0700

    Allow the Car VIN to be optional/

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ApplicationRecord
 
   validates :mileage, { presence: true }
-  validates :vin, length: { is: 17 }
+  validates :vin, length: { is: 17 }, allow_blank: true
   validates :vin, format: { with: /\A[a-zA-Z0-9_]{17}\z/ }
 
 end
