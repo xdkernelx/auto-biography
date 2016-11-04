f932a7df5d183179d75a9aa94f4f30ef7ac3f66b
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-04 15:22:41 -0700

    Modify the rspec to destroy all the instances inside the database for continuous testing.

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -3,18 +3,20 @@ require "rails_helper"
 RSpec.describe Car, :type => :model do
 
   before(:all) do
-    @lindeman = User.create!(first_name: "Andy", last_name: "Lindeman", password: "password", email: "tester@test.com")
-    @star = Car.create!(user_id: 1, mileage: 100, vin: "11111111111111111")
-    @dust = Car.create!(user_id: 1, mileage: 20, vin: "11111111111111119")
-  end
-
-  it "creates correct amount based on before call" do
-    expect(Car.count).to eq(2)
+    User.destroy_all
+    Car.destroy_all
+    @lindeman = User.create(first_name: "Andy", last_name: "Lindeman", password: "password", email: "tester@test.com")
+    @star = Car.create(user_id: @lindeman.id, mileage: 100, vin: "11111111111111111")
+    @dust = Car.create(user_id: @lindeman.id, mileage: 20, vin: "11111111111111119")
   end
 
   it "correctly associates the user" do
-    p @star.user
     expect(@star.user).to eq(@lindeman)
+  end
+
+  it "return false if the VIN isn't alphanumeric" do
+    @test = Car.new(user_id: @lindeman.id, mileage: 100, vin: "333333FFFFFF6666$")
+    expect( @test.save ).to eq(false)
   end
 
 end
