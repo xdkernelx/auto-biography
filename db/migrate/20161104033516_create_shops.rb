ffd42d65e1ecc4e645e6b39cbec489353e929c74
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 20:41:15 -0700

    Remove the change_table from the Model folder into the DB folder.

--- db/migrate/20161104033516_create_shops.rb
+++ db/migrate/20161104033516_create_shops.rb
@@ -1,8 +1,15 @@
 class CreateShops < ActiveRecord::Migration[5.0]
   def change
     create_table :shops do |t|
+      t.integer :owner_id
+      t.string :name, { limit: 64 }
+      t.string :summary
+      t.string :specialties
+      t.string :address, { limit: 128 }
+      t.string :city, { limit: 64 }
+      t.string :state, { limit: 2 }
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
