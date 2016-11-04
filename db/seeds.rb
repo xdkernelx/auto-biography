9bb1856f1bbccbe7bf70beedb87b45a657893a92
Paula Lee <pdlee877@gmail.com>
2016-11-04 15:38:23 -0700

    testing repair route still has car_id and issue_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(first_name: "Sponge", last_name: "Bob", phone: "222-333-4444", password: "password")
+c1 = Car.create(vin: "3VWSE69M35M029058", mileage: 32555)
+i1 = Issue.create(title: "engine light on", description: "engine light turned on today", urgency: 4)
+r1 = Repair.create(title: "fixed engine light", description: "did not turn gas cap all the way", mileage: 32570, date_completed: "11/4/2016")
