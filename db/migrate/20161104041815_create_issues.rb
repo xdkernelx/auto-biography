1cd3bcc2d092b6673104ff305b551a44fba2337b
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 21:20:36 -0700

    Create fields for the Issue model migration.

--- db/migrate/20161104041815_create_issues.rb
+++ db/migrate/20161104041815_create_issues.rb
@@ -1,6 +1,9 @@
 class CreateIssues < ActiveRecord::Migration[5.0]
   def change
     create_table :issues do |t|
+      t.string :title, { limit: 64 }
+      t.string :description
+      t.integer :urgency
 
       t.timestamps
     end
