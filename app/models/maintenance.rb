18dd46f9fb217a0a2364ca8aabf84b2eca192391
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-04 16:31:15 -0700

    Update validations for the mileage on both the Repair and Maintenance class.

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -3,5 +3,6 @@ class Maintenance < ApplicationRecord
   belongs_to :car
 
   validates :title, :description, :mileage, :date_completed, { presence: true }
+  validates :mileage, numericality: { greater_than: 0 }
 
 end
--- app/models/repair.rb
+++ app/models/repair.rb
@@ -3,5 +3,6 @@ class Repair < ApplicationRecord
   belongs_to :issue
 
   validates :title, :description, :mileage, :date_completed, { presence: true }
+  validates :mileage, numericality: { greater_than: 0 }
 
 end
