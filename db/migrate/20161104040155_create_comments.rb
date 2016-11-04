271cf15ce9d39b0b0b6c2b59bc72a62803c41ab0
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 21:09:23 -0700

    Remove the commentable_type field inside the CreateComments.

--- db/migrate/20161104040155_create_comments.rb
+++ db/migrate/20161104040155_create_comments.rb
@@ -1,6 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
+      t.integer :commentable_id
+      t.string :body
 
       t.timestamps
     end
