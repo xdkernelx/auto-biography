3ba3943bec3a276165473efb85d5bdfc5838daec
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 20:49:46 -0700

    Write the migration fields for the ReportTypes model.

--- db/migrate/20161104034755_create_report_types.rb
+++ db/migrate/20161104034755_create_report_types.rb
@@ -1,6 +1,8 @@
 class CreateReportTypes < ActiveRecord::Migration[5.0]
   def change
     create_table :report_types do |t|
+      t.string :type, { limit: 32 }
+      t.string :name, { limit: 64 }
 
       t.timestamps
     end
