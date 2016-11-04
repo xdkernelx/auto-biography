83a4d5ee60aeebdaa65e141070f5c54718e9b9a4
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 20:28:06 -0700

    Add presence validation for several fields in the User class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, :phone, { presence: true }
+
 end
