54e23e86f98ba1cf4e4cc3a3af4f09f8b44fa9fe
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 20:46:20 -0700

    Write migration file for our Ticket model.

--- db/migrate/20161104034405_create_tickets.rb
+++ db/migrate/20161104034405_create_tickets.rb
@@ -1,6 +1,11 @@
 class CreateTickets < ActiveRecord::Migration[5.0]
   def change
     create_table :tickets do |t|
+      t.integer :user_id
+      t.string :title, { limit: 64 }
+      t.string :description
+      t.boolean :open, { default: true }
+      t.boolean :acces, { default: false }
 
       t.timestamps
     end
