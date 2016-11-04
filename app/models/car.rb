170c02296f2b79bbe4efb11723840abe04d39c59
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 21:14:27 -0700

    Write the fields for the Car model.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,4 @@
 class Car < ApplicationRecord
+
+
 end
--- db/migrate/20161104041222_create_cars.rb
+++ db/migrate/20161104041222_create_cars.rb
@@ -1,6 +1,9 @@
 class CreateCars < ActiveRecord::Migration[5.0]
   def change
     create_table :cars do |t|
+      t.integer :vin
+      t.integer :user_id
+      t.integer :mileage
 
       t.timestamps
     end
