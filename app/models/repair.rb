14eba582bccfb0bf3211fd5c1ad4e529ef1871a9
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-04 14:25:35 -0700

    Modify the model and migration files for the Repair report class.

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -1,2 +1,5 @@
 class Repair < ApplicationRecord
+
+  validates :title, :description, :mileage, :date_completed, { presence: true }
+
 end
--- db/migrate/20161104212052_create_repairs.rb
+++ db/migrate/20161104212052_create_repairs.rb
@@ -1,6 +1,13 @@
 class CreateRepairs < ActiveRecord::Migration[5.0]
   def change
     create_table :repairs do |t|
+      t.integer :issue_id
+      # t.integer :repairable_id
+      t.string :title, { limit: 64 }
+      t.string :description
+      t.integer :mileage
+      t.date :date_completed
+      # t.integer :rating
 
       t.timestamps
     end
