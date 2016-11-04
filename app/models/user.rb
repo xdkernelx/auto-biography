ad6a24184decce4314393b595bdfa5b82dbc9cf3
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-03 20:30:49 -0700

    Remove phone presence validation, but add a validation that makes sure that the length of the given phone is 10 to include the area code in US format.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, :last_name, :phone, { presence: true }
+  validates :first_name, :last_name, { presence: true }
+  validates :phone, length: { is: 5 }, allow_blank: true
 
 end
