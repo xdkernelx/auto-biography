d837be15cc098f4edb5805b05e0dde6b1dc31378
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-04 18:50:10 -0700

    Create simple tests that test whether we can create valid Maintenance reports. Testing for presence fields seems a bit needless since AR already checks if the thing it's validating is existing and would be inefficient during runtime, especially since we are implementing Travis. Not exactly a shortcut.

--- spec/models/maintenance_spec.rb
+++ spec/models/maintenance_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Maintenance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    User.destroy_all
+    Car.destroy_all
+    Maintenance.destroy_all
+    @lindeman = User.create(first_name: "Andy", last_name: "Lindeman", password: "password", email: "tester@test.com")
+    @star = Car.create(user_id: @lindeman.id, mileage: 100, vin: "11111111111111111")
+    @dust = Car.create(user_id: @lindeman.id, mileage: 20, vin: "11111111111111119")
+    @oil_change = Maintenance.create(car_id: @star.id, title: "Scheduled Oil Change", description: "See Title", mileage: 200, date_completed: "11/04/2016")
+    @alignment = Maintenance.create(car_id: @star.id, title: "Scheduled Alignment", description: "Off by 1 degree", mileage: 200, date_completed: "11/04/2016")
+  end
+
+  it "allows multiple maintenance to be created" do
+    expect(Maintenance.count).to eq(2)
+  end
+
+  it "doesn't allow a mileage to be 0 or below" do
+    @test = Maintenance.new(car_id: @star.id, title: "Scheduled Alignment", description: "Off by 1 degree", mileage: 0, date_completed: "11/04/2016")
+    expect(@test.valid?).to eq(false)
+    @test = Maintenance.new(car_id: @star.id, title: "Scheduled Alignment", description: "Off by 1 degree", mileage: -1, date_completed: "11/04/2016")
+    expect(@test.valid?).to eq(false)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe User, :type => :model do
     @tail_light = Issue.create(car_id: @star.id, title: "Engine won't turn on", description: "See Title", open: true)
     @tail_light_fix = Repair.create(issue_id: @tail_light.id, title: "Tail Light Wiring", description: "Tail light wiring messing with the engine", mileage: 3200, date_completed: "11/04/2016")
     @engine_fix = Repair.create(issue_id: @engine.id, title: "Engine dead", description: "Engine is dead. Please buy a new one", mileage: 3200, date_completed: "11/04/2016")
-
   end
 
   it "allows the phone entry to be blank" do
