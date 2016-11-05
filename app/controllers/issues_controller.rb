27daccbc3c8aa9d3cf0dbcba59c76e4e18ec2bad
Mal Wood <mallorywood413@gmail.com>
2016-11-05 16:17:18 -0700

    Update issues_controller.rb

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,58 +1,59 @@
 class IssuesController < ApplicationController
 
-	def index
-		@car = Car.find(params[:car_id])
-		@issues = @car.issues
-	end
+  def index
+    @car = Car.find(params[:car_id])
+    @issues = @car.issues
+  end
 
-	def show
-		@issue = Issue.find(params[:id])
-		@car = Car.find(params[:car_id])
-		@repairs = @issue.repairs 
-	end
+  def show
+    @issue = Issue.find(params[:id])
+    @car = Car.find(params[:car_id])
+    @repairs = @issue.repairs 
+  end
 
-	def new
-		@car = Car.find(params[:car_id])
-		@issue = Issue.new
-	end 
+  def new
+    @car = Car.find(params[:car_id])
+    @issue = Issue.new
+  end 
 
-	def edit
-		@issue = Issue.find(params[:id])
-		@car = Car.find(params[:car_id])
-	end
+  def edit
+    @issue = Issue.find(params[:id])
+    @car = Car.find(params[:car_id])
+  end
 
-	def create
-		@issue = Issue.new(issue_params)
-		@car = Car.find(params[:car_id])
-		if @issue.save
-			redirect_to car_issues_path(@car)
-		else
-			@errors = @issue.errors.full_messages
-			render 'new'
-		end
-	end
+  def create
+  @issue = Issue.new(issue_params)
+  @car = Car.find(params[:car_id])
+  if @issue.save
+    redirect_to car_issues_path(@car)
+  else
+    @errors = @issue.errors.full_messages
+    render 'new'
+  end
+  end
 
-	def update
-		@issue = Issue.find(params[:id])
-		@car = Car.find(params[:car_id])
-			if @issue.update(issue_params)
-				redirect_to car_issues_path(@car)
-			else
-				@errors = @issue.errors.full_messages
-				render 'edit'
-		end
-	end
+  def update
+    @issue = Issue.find(params[:id])
+    @car = Car.find(params[:car_id])
+    if @issue.update(issue_params)
+	redirect_to car_issues_path(@car)
+    else
+	@errors = @issue.errors.full_messages
+	render 'edit'
+    end
+  end
 
-	def destroy 
-		@issue = Issue.find(params[:id])
-		@car = Car.find(params[:car_id])
-		@issue.destroy
-		redirect_to car_issues_path(@car)
-	end 
-	private
+  def destroy 
+    @issue = Issue.find(params[:id])
+    @car = Car.find(params[:car_id])
+    @issue.destroy
+    redirect_to car_issues_path(@car)
+  end 
+	
+  private
 
-	def issue_params
-		params.require(:issue).permit(:title, :description, :urgency, :car_id)
-	end
+  def issue_params
+    params.require(:issue).permit(:title, :description, :urgency, :car_id)
+  end
 
 end
