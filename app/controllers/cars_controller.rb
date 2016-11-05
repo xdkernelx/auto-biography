d0ca1b2348a6fede5b99f0771e794ea945aa5ab7
marvan_t <marvan_t@hotmail.com>
2016-11-04 18:14:59 -0700

    add new car form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,4 +8,26 @@ class CarsController < ApplicationController
 		@car = Car.find(params[:id])
 	end
 
+	def new
+		@car = Car.new
+	end 
+
+	def edit
+	end
+
+	def create
+		@car = Car.new(car_params)
+		p '*************************************'
+		@user = current_user
+		p '*************************************'
+		@car.user_id = @user.id
+		p '*************************************'
+		@car.save
+		redirect_to cars_path
+	end
+
+	private
+	def car_params
+		params.require(:car).permit(:user_id, :mileage, :vin, :year, :make, :model, :color, :transmission, :engine)
+	end
 end
