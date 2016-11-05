f51c665ecb7fadbe79dde64a66a742a407b75427
mal-wood <mallorywood413@gmail.com>
2016-11-05 12:13:08 -0700

    Fixed logic for issues form

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,11 +1,30 @@
 class IssuesController < ApplicationController
+
 	def index
 		@car = Car.find(params[:car_id])
 		@issues = @car.issues
-		p params
 	end
 
 	def new
+		@car = Car.find(params[:car_id])
 		@issue = Issue.new
 	end 
+
+	def create
+		@car = Car.find(params[:car_id])
+		@issue = Issue.new(issue_params)
+		if @issue.save
+			redirect_to car_issues_path(@car)
+		else
+			@errors = @issue.errors.full_messages
+			render 'new'
+		end
+	end
+
+	private
+
+	def issue_params
+		params.require(:issue).permit(:title, :description, :urgency, :car_id)
+	end
+
 end
