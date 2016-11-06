f4dd81276c31ca90c1255c2e449617dad973cbe1
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-06 14:49:44 -0800

    Currently working code.

--- app/models/car.rb
+++ app/models/car.rb
@@ -9,5 +9,5 @@ class Car < ApplicationRecord
   validates :mileage, { presence: true }
   validates :mileage, numericality: { greater_than: 0 }
   validates :vin, length: { is: 17 }, allow_blank: true
-  validates :vin, format: { with: /\A[a-zA-Z0-9_]{17}\z/ }
+  validates :vin, format: { with: /\A[a-zA-Z0-9_]{17}\z/ }, allow_blank: true
 end
