2a80b02114bad0380c0c7b47acaf7b528b12f4ab
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-07 14:32:49 -0800

    Add tests that make sure there is a difference between maintenances through a car and non-specific to a car.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,9 @@ RSpec.describe User, :type => :model do
     @back_light = Issue.create(car_id: @dust.id, title: "Engine won't turn on", description: "See Title")
     @tail_light_fix = Repair.create(issue_id: @tail_light.id, mechanic_id: @travis.id, title: "Tail Light Wiring", description: "Tail light wiring messing with the engine", mileage: 3200, date_completed: "11/04/2016")
     @engine_fix = Repair.create(issue_id: @engine.id, mechanic_id: @travis.id, title: "Engine dead", description: "Engine is dead. Please buy a new one", mileage: 3200, date_completed: "11/04/2016")
+    @oil_change = Maintenance.create(car_id: @star.id, mechanic_id: @travis.id, title: "Scheduled Oil Change", description: "See Title", mileage: 200, date_completed: "11/04/2016")
+    @alignment = Maintenance.create(car_id: @star.id, mechanic_id: @travis.id, title: "Scheduled Alignment", description: "Off by 1 degree", mileage: 200, date_completed: "11/04/2016")
+    @tire_change = Maintenance.create(car_id: @dust.id, mechanic_id: @travis.id, title: "Scheduled Alignment", description: "Off by 1 degree", mileage: 200, date_completed: "11/04/2016")
   end
 
   context "user creation" do
@@ -33,7 +36,7 @@ RSpec.describe User, :type => :model do
       expect(@lindeman.cars).to match_array([@star, @dust])
     end
 
-    it "has an association with it's issues through cars" do
+    it "has an association with it's issues through a specific car" do
       expect(@lindeman.cars.find(@star.id).issues).to match_array([@engine, @tail_light])
     end
 
@@ -41,12 +44,20 @@ RSpec.describe User, :type => :model do
       expect(@lindeman.issues).to match_array([@engine, @tail_light, @back_light])
     end
 
-    it "has an association with it's repairs through issues" do
+    it "has an association with it's repairs through issues specific to a car" do
       expect(@lindeman.cars.find(@star.id).issues.find(@tail_light.id).repairs).to match_array([@tail_light_fix])
     end
 
     it "differentiates the child association of repairs non-specific to a car" do
       expect(@lindeman.repairs).to match_array([@tail_light_fix, @engine_fix])
+    end
+
+    it "has an association with it's maintenances specific to a car" do
+      expect(@lindeman.cars.find(@star.id).maintenances).to match_array([@oil_change, @alignment])
+    end
+
+    it "differentiates the child associations non-specific to a car" do
+      expect(@lindeman.maintenances).to match_array([@oil_change, @alignment, @tire_change])
     end
   end
 
