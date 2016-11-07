b7bf14ce2893165b5774f56916529fc5eeed9058
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-06 16:01:35 -0800

    Make it so that the user can be locked out of account if they fail 5 attempts. Can only be reset if the user waits the alloted time. TODO: Need to figure out how to display the message when there is only 1 attempt left.

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20161104212052) do
     t.datetime "updated_at",                                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
 end
