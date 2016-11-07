b96cee77f1416973c51140fbda4720587f71de6c
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-07 10:04:33 -0800

    Add custom validation to make sure that the mechanic or owner don't modify the report in a way that make it so that the mileage is lower than the initial state of the car's inherent mileage.

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -6,6 +6,7 @@ class Repair < ApplicationRecord
   validates :title, :description, :mileage, :date_completed, { presence: true }
   validates :mileage, numericality: { greater_than: 0 }
   validate :mechanic_status
+  validate :authenticate_mileage
 
   def mechanic_status
 
@@ -17,6 +18,21 @@ class Repair < ApplicationRecord
       user = User.find(self.mechanic_id)
       if (!user.mech_status)
         errors.add(:mechanic_id, "id must belong to a mechanic user.")
+      end
+    end
+
+  end
+
+  def authenticate_mileage
+
+    begin
+      issue = Issue.find(self.issue_id)
+    rescue
+      errors.add(:issue_id, "Could not find associated issue.")
+    else
+      issue = Issue.find(self.issue_id)
+      if (self.mileage < issue.car.mileage)
+        errors.add(:mileage, "mileage cannot be lower than the current state.")
       end
     end
 
