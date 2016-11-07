fb000b9b3bd3a929c85ea29304f46a11e8a29c4a
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-07 09:39:28 -0800

    Add a custom validation that checks whether the given mechanic_id belongs to a mechanic User.

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -6,5 +6,21 @@ class Maintenance < ApplicationRecord
 
   validates :title, :description, :mileage, :date_completed, { presence: true }
   validates :mileage, numericality: { greater_than: 0 }
+  validate :mechanic_status
+
+  def mechanic_status
+
+    begin
+      user = User.find(self.mechanic_id)
+    rescue
+      errors.add(:mechanic_id, "Could not find user.")
+    else
+      user = User.find(self.mechanic_id)
+      if (!user.mech_status)
+        errors.add(:mechanic_id, "id must belong to a mechanic user.")
+      end
+    end
+
+  end
 
 end
