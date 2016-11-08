ae69b20d7b93bd5cd585ab98baed7ca5f1737050
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-08 02:21:13 -0800

    Add validations for the Shop class.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,8 @@
 class Shop < ApplicationRecord
+
+  validates :name, :address, :city, :state, :zip_code, { presence: true }
+  validates :state, format: { with: /\A[0-9]{2}\z/ }
+  validates :zip_code, format: { with: /\A[0-9]{5}\z/ }
+  validates :phone, format: { with: /\A[0-9]{10}\z/ }, allow_blank: true
+
 end
