ca6145bdf9808aa2bb2cea88b53cb7c08a02fd3b
mal-wood <mallorywood413@gmail.com>
2016-11-07 23:28:04 -0800

    Add car model methods to display five most recent

--- app/models/car.rb
+++ app/models/car.rb
@@ -13,12 +13,15 @@ class Car < ApplicationRecord
   validates :vin, format: { with: /\A[a-zA-Z0-9]{17}\z/ }, allow_blank: true
 
   def recent_maintenances(limit)
-   self.maintenances.sort_by{|maintenance| maintenance.date_completed}.take(limit)
+   self.maintenances.sort_by{|maintenance| maintenance.date_completed}.reverse!.take(limit)
   end 
 
   def recent_repairs(limit)
-   self.repairs.sort_by{|repair| repair.date_completed}.take(limit)
+   self.repairs.sort_by{|repair| repair.date_completed}.reverse!.take(limit)
   end 
 
+  def recent_issues(limit)
+    self.issues.sort_by{|issue| issue.updated_at}.reverse!.take(limit)
+  end 
 
 end
