31d2f7fb7f7c29c496a6e760e4bec80362568825
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-08 03:27:47 -0800

    Write fields for Comment migration.

--- db/migrate/20161108112546_create_comments.rb
+++ db/migrate/20161108112546_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
+      t.string :title, { limit: 64 }
+      t.string :body
+      t.integer :commentable_id
 
       t.timestamps
     end
