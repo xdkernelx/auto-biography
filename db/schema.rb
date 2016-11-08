7ff85adec7c6936b1f377024891ab1e4c7050861
mal-wood <mallorywood413@gmail.com>
2016-11-07 18:53:37 -0800

    Updated css for map page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104212052) do
+ActiveRecord::Schema.define(version: 20161108012540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 20161104212052) do
     t.integer  "rating"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "permissions", force: :cascade do |t|
+    t.integer  "car_id"
+    t.integer  "issue_id"
+    t.string   "type",       limit: 11
+    t.string   "token"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   create_table "repairs", force: :cascade do |t|
