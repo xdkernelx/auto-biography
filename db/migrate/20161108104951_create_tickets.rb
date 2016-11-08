b59087e3ab916c3b22c5464205c03c4267699b45
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-08 02:52:33 -0800

    Write needed fields for the Ticket model.

--- db/migrate/20161108104951_create_tickets.rb
+++ db/migrate/20161108104951_create_tickets.rb
@@ -1,6 +1,11 @@
 class CreateTickets < ActiveRecord::Migration[5.0]
   def change
     create_table :tickets do |t|
+      t.string :title, { limit: 64 }
+      t.string :description
+      t.boolean :public_acces, { default: false }
+      t.integer :user_id
+      t.integer :car_id
 
       t.timestamps
     end
