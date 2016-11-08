8c8dc6f1dffb664eb11e210eb4da9165ccad1595
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-08 15:22:45 -0800

    Create helper method to generate a URL token.

--- app/helpers/specials_helper.rb
+++ app/helpers/specials_helper.rb
@@ -1,24 +1,28 @@
 module SpecialsHelper
 
-  def generate_token(type, car)
-    if type.class.name == 'Issue'
+  def generate_url(type, car)
+    if current_user.id != car.user_id
+      nil
+    elsif type.class.name == 'Issue'
       @token = SecureRandom.urlsafe_base64
       @permission = Permission.new(car_id: car.id, issue_id: type.id, report_type: type.class.name, token: @token)
       if !@permission.save
-        return nil
+        nil
       else
-        return @token
+        # request.host_with_port.to_s + "/specials/?&report_type=#{type.class.name}&issue_id=#{type.id}&car_id=#{car.id}&token=#{@token}"
+        "localhost:3000" + "/specials/?&report_type=#{type.class.name}&issue_id=#{type.id}&car_id=#{car.id}&token=#{@token}"
       end
     elsif type.class.name == 'Maintenance'
       @token = SecureRandom.urlsafe_base64
       @permission = Permission.new(car_id: car.id, report_type: type.class.name, token: @token)
       if !@permission.save
-        return nil
+        nil
       else
-        return @token
+        # request.host_with_port.to_s + "/specials/?&report_type=#{type.class.name}&car_id=#{car.id}&token=#{@token}"
+        "localhost:3000" + "/specials/?&report_type=#{type.class.name}&car_id=#{car.id}&token=#{@token}"
       end
     else
-      return nil
+      nil
     end
   end
 
