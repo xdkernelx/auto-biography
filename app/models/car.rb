00b9b81714491895f89997bfc8a8312f57145d8c
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-08 11:43:11 -0800

    Add helper method to return all the repairs specific to the car while differentiating between repairs associated through either the ticket model and the issue model.

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,22 +3,28 @@ require 'securerandom'
 class Car < ApplicationRecord
 
   has_many :issues
-  has_many :repairs, through: :issues
+  has_many :issue_repairs, through: :issues, source: :repairs
+  has_many :ticket_repairs, through: :tickets, source: :repairs
   has_many :maintenances
   has_many :permissions
-  has_one :ticket
+  has_many :tickets
   belongs_to :user
 
   validates :mileage, { presence: true }
   validates :mileage, numericality: { greater_than: 0 }
   validates :vin, format: { with: /\A[a-zA-Z0-9]{17}\z/ }, allow_blank: true
 
+
+  def repairs
+    self.issue_repairs + self.ticket_repairs
+  end
+
   def recent_maintenances(limit)
     self.maintenances.sort_by{|maintenance| maintenance.date_completed}.reverse!.take(limit)
   end
 
   def recent_repairs(limit)
-    self.repairs.sort_by{|repair| repair.date_completed}.reverse!.take(limit)
+    repairs.sort_by{|repair| repair.date_completed}.reverse!.take(limit)
   end
 
   def recent_issues(limit)
