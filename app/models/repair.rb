b769249f810a40f871e0832ab81be48bb0cd89e5
Paula Lee <pdlee877@gmail.com>
2016-11-08 20:53:01 -0800

    fix edit-update in the repairs controller, took out the contents of the head in the cars show file for the d3 things but left the javascript and removed the js file

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -6,7 +6,7 @@ class Repair < ApplicationRecord
 
   validates :title, :description, :mileage, :date_completed, { presence: true }
   validates :mileage, numericality: { greater_than: 0 }
-  validates :shop_status, optional: true
+  validate :shop_status
 
   def shop_status
     if self.shop_id
