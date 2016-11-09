ee65c9b1c5345a1d973e7dbd7b562ca052040544
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-09 14:43:20 -0800

    Implement the service? helper method and update rspecs to ensure functionality.

--- spec/models/car_update_spec.rb
+++ spec/models/car_update_spec.rb
@@ -14,11 +14,12 @@ RSpec.describe Car, :type => :model do
     Comment.destroy_all
     @lindeman = User.create(first_name: "Andy", last_name: "Lindeman", password: "password", email: "tester@test.com")
     @travis = User.create(first_name: "Bob", last_name: "Builder", password: "password", email: "tester2@test.com", mech_status: true)
-    @star = Car.create(user_id: @lindeman.id, mileage: 7000, vin: "11111111111111111")
+    @star = Car.create(user_id: @lindeman.id, mileage: 61000, vin: "11111111111111111")
     @dust = Car.create(user_id: @lindeman.id, mileage: 20, vin: "11111111111111119")
     @matter = Car.create(user_id: @lindeman.id, mileage: 20, vin: "11111111111111119")
     @oreilly = Shop.create(name: "O'Reilly", mechanic_id: @travis.id, address: "636 Spruce St.", city: "San Francisco", state: "CA", zip_code: "94118")
-    @oil_change = Maintenance.create(car_id: @star.id, shop_id: @oreilly.id, title: "oil change", description: "See Title", mileage: 6000, date_completed: "11/04/2016")
+    @oil_change = Maintenance.create(car_id: @star.id, shop_id: @oreilly.id, title: "oil change", description: "See Title", mileage: 60000, date_completed: "11/04/2016")
+    @service = Maintenance.create(car_id: @star.id, shop_id: @oreilly.id, title: "service", description: "See Title", mileage: 60000, date_completed: "11/04/2016")
     @alignment = Maintenance.create(car_id: @star.id, shop_id: @oreilly.id, title: "Scheduled Alignment", description: "Off by 1 degree", mileage: 200, date_completed: "11/05/2016")
 end
 
@@ -34,8 +35,14 @@ end
   end
 
   context "Car.oil_change?" do
-    it "- returns 1000 if the Car.mileage is 7000 and the recent 'oil change' maintenance is 6000" do
+    it "- returns 1000 if the Car.mileage is 61000 and the recent 'oil change' maintenance is at 60000" do
       expect(@star.oil_change?).to eq(1000)
+    end
+  end
+
+  context "Car.service?" do
+    it "- returns 1000 if the Car.mileage 61000 and the most recent 'service' maintenance is at 60000" do
+      expect(@star.service?).to eq(1000)
     end
   end
 
