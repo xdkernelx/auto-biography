ada13a59b59c22760fba2f2ce3526d9335c47790
Paula Lee <pdlee877@gmail.com>
2016-11-09 02:56:16 -0800

    adding maintenance to the specials route part 1 of 3 done

--- app/controllers/specials_controller.rb
+++ app/controllers/specials_controller.rb
@@ -1,11 +1,12 @@
 class SpecialsController < ApplicationController
 
 	def index
+		# need to test if moving the issue_id inside the if statement effects anything
 		@report_type = params[:report_type] if params[:report_type]
-		@issue_id = params[:issue_id] if params[:issue_id]
 		@car_id = params[:car_id] if params[:car_id]
 		@token = params[:token] if params[:token]
 		if @report_type == "Issue"
+			@issue_id = params[:issue_id] if params[:issue_id]
 			@repair = Repair.new
 			@repair.repairable = Issue.find(params[:issue_id])
 			p '*' * 100
@@ -19,11 +20,12 @@ class SpecialsController < ApplicationController
 	end
 
 	def create
+		# NEED TO SORT THIS TO INCLUDE MAINTENANCE
+		# already added form to index
+		# need to include maintenance strong_params 
+
 		@car = Car.find(params[:repair][:car_id])
 		@issue = Issue.find(params[:repair][:issue_id])
-		p '*' * 80
-		p @issue
-		p '*' * 80
 		@repair = Repair.new(repair_params)
 		@repair.repairable = @issue
 		
@@ -32,6 +34,7 @@ class SpecialsController < ApplicationController
 		else
 			@errors = @repair.errors.full_messages
 			redirect_to request.original_url
+			# need to fix the redirect_to
 		end
 	end
 
