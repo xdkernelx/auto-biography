b0b7499a5e70d360855326c7565eae95cae699c9
Paula Lee <pdlee877@gmail.com>
2016-11-10 22:20:38 -0800

    after new issue redirects to car show page

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -23,7 +23,7 @@ class IssuesController < ApplicationController
     @issue = Issue.new(issue_params)
     @car = Car.find(params[:car_id])
       if @issue.save
-        redirect_to car_issues_path(@car)
+        redirect_to car_path(@car)
       else
         @errors = @issue.errors.full_messages
       render 'new'
