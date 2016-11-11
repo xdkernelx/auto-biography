9542cecac01e1eac0c53fd5b7e54dd98cd7338aa
mal-wood <mallorywood413@gmail.com>
2016-11-11 10:25:05 -0800

    change heroku link to localhost on specials controller

--- app/helpers/specials_helper.rb
+++ app/helpers/specials_helper.rb
@@ -11,8 +11,8 @@ module SpecialsHelper
         nil
       else
         # request.host_with_port.to_s + "/specials/?&report_type=#{type.class.name}&issue_id=#{type.id}&car_id=#{car.id}&token=#{@token}"
-        issue_tokens.push("http://auto-biography.herokuapp.com" + "/specials/?&report_type=#{type.class.name}&issue_id=#{type.id}&car_id=#{car.id}&token=#{@token}")
-        issue_tokens.push("http://auto-biography.herokuapp.com" + "/cars/#{car.id}/issues/#{type.id}/?&token=#{@token}")
+        issue_tokens.push("localhost:3000" + "/specials/?&report_type=#{type.class.name}&issue_id=#{type.id}&car_id=#{car.id}&token=#{@token}")
+        issue_tokens.push("localhost:3000" + "/cars/#{car.id}/issues/#{type.id}/?&token=#{@token}")
         issue_tokens
       end
     elsif type.class.name == 'Maintenance'
@@ -22,8 +22,8 @@ module SpecialsHelper
         nil
       else
         # request.host_with_port.to_s + "/specials/?&report_type=#{type.class.name}&car_id=#{car.id}&token=#{@token}"
-        issue_tokens.push("http://auto-biography.herokuapp.com" + "/specials/?&report_type=#{type.class.name}&car_id=#{car.id}&token=#{@token}")
-        issue_tokens.push("http://auto-biography.herokuapp.com" + "/cars/#{car.id}/maintenances/?&token=#{@token}")
+        issue_tokens.push("localhost:3000" + "/specials/?&report_type=#{type.class.name}&car_id=#{car.id}&token=#{@token}")
+        issue_tokens.push("localhost:3000" + "/cars/#{car.id}/maintenances/?&token=#{@token}")
         issue_tokens
       end
     else
