1bfd1b0e6f174f3d65951b95bdcb7ce0efd71e2b
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-10 23:57:13 -0800

    Refactor code to include Radix parameter.

--- app/assets/javascripts/liquidFillGauge.js
+++ app/assets/javascripts/liquidFillGauge.js
@@ -34,9 +34,9 @@ function loadLiquidFillGauge(elementId, value, config) {
     if(config === null) config = liquidFillGaugeDefaultSettings();
 
     var gauge = d3.select("#" + elementId);
-    var radius = Math.min(parseInt(gauge.style("width")), parseInt(gauge.style("height")))/2;
-    var locationX = parseInt(gauge.style("width"))/2 - radius;
-    var locationY = parseInt(gauge.style("height"))/2 - radius;
+    var radius = Math.min(parseInt(gauge.style("width"), 10), parseInt(gauge.style("height"), 10))/2;
+    var locationX = parseInt(gauge.style("width"), 10)/2 - radius;
+    var locationY = parseInt(gauge.style("height"), 10)/2 - radius;
     var fillPercent = Math.max(config.minValue, Math.min(config.maxValue, value))/config.maxValue;
 
     var waveHeightScale;
